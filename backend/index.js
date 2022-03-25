5aa7fd8de6ea03d5088db3d12fed6a7c7d512082
Elin Svensell <elin@Elins-MacBook-Air.local>
2022-03-26 00:11:12 +0100

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,6 @@ app.get("/getData", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
