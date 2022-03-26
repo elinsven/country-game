b95c456b2ec7158a6ef12352e90bfc44e2dbc49f
Elin Svensell <elin@Elins-MacBook-Air.local>
2022-03-26 02:06:13 +0100

    kanske deployar nu

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
+const pool = new Pool(process.env.DATABASE_URL, {
     user: "postgres",
     password: "3431",
     database: "countries",
